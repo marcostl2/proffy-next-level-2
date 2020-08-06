30d9bedf35807ce96b6c5dd5ef2c60725e2cb638
marcostl2 <marcossilvatl2@gmail.com>
2020-08-06 03:48:43 +0000

    initial page finalized

--- proffy/web/src/pages/Landing/index.tsx
+++ proffy/web/src/pages/Landing/index.tsx
@@ -9,26 +9,30 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 
 import './styles.scss'
 
-function Landing(){
-    return(
+function Landing() {
+    return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
-                <div className="logo-container">
-                    <img src={logoImg} alt="Proffy"/>
-                    <h2>Sua plataforma de estudos online.</h2>
+                <div className="logo-wrapper container">
+                    <div className="logo-container">
+                        <img src={logoImg} alt="Proffy" />
+                        <h2>Sua plataforma de estudos online.</h2>
+                    </div>
+                    <img src={landingImg} alt="Plataforma de Estudos" className="hero-image" />
                 </div>
-                <img src={landingImg} alt="Plataforma de Estudos" className="hero-image"/>
-                <div className="buttons-container">
-                    <a href="" className="study">
-                        <img src={studyIcon} alt="Estudar"/>
-                        Estudar
-                    </a>
-                    <a href="" className="give-classes">
-                        <img src={giveClassesIcon} alt="Estudar"/>
-                        Estudar
-                    </a>
+                <div className="buttons-wrapper">
+                    <div className="buttons-container">
+                        <a href="" className="study">
+                            <img src={studyIcon} alt="Estudar" />
+                            Estudar
+                        </a>
+                        <a href="" className="give-classes">
+                            <img src={giveClassesIcon} alt="Dar aulas" />
+                            Dar aulas
+                        </a>
+                    </div>
                     <span className="total-connections">
-                        Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+                        Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
                     </span>
                 </div>
             </div>
---
+++ proffy/web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TeacherForm(){
+    return(
+        <div></div>
+    )
+}
+
+export default TeacherForm
